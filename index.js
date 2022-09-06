a45d8f71b425038476347c307accadd6c2b12262
Lumenatom <Lumenatom@gmail.com>
2022-09-06 10:40:59 +0300

    edit style

--- index.js
+++ index.js
@@ -12,3 +12,29 @@ for (let anchor of anchors) {
         })
     })
 }
+
+const imageBlock = document.querySelector(".images");
+const leftArrow = document.querySelector(".left-arrow");
+const rightArrow = document.querySelector(".right-arrow");
+let offset = 0;
+let width;
+const slideLeft = () => {
+    width = imageBlock.offsetWidth;
+    offset = offset + width;
+    if (offset >= width * 2) {
+        offset = 0;
+    }
+    imageBlock.style.left = -offset + "px";
+}
+const slideRight = () => {
+    width = imageBlock.offsetWidth;
+    offset = offset - width;
+    if (offset < 0) {
+        offset = width;
+    }
+    imageBlock.style.left = -offset + "px";
+}
+
+
+leftArrow.addEventListener("click", slideLeft);
+rightArrow.addEventListener("click", slideRight);
